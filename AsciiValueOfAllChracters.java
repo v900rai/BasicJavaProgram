34736f5f3d1914ffac5672e0070bde6a7ffbf406
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-11-25 13:33:53 +0530

    Add files via upload

---
+++ AsciiValueOfAllChracters.java
@@ -0,0 +1,20 @@
+package edu.jspider.heca48.twoD.Array;
+
+public class AsciiValueOfAllChracters 
+{
+	public static void main(String []args)
+	{
+		for(int i = 0; i <= 255; i++)  
+		{  
+		System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+		break;
+		} 
+		
+		
+		for(int i = 65; i <= 90; i++)  
+		{  
+		System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+		}  
+	}
+
+}
