6587755abed08039481dab5334ba9e563368ec86
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-01-14 13:28:22 +0530

    Add files via upload

---
+++ usedthridvariable.java
@@ -0,0 +1,13 @@
+package DSA;
+
+public class usedthridvariable 
+{
+	public static void main(String []args)
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Result:--"+c);
+	}
+
+}
