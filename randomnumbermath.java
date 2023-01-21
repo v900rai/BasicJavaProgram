a52dc65ceab1ecafbf415b01427627296390effc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-01-22 00:51:01 +0530

    Add files via upload

---
+++ randomnumbermath.java
@@ -0,0 +1,14 @@
+package DSA;
+import java.lang.Math;
+public class randomnumbermath 
+{
+	public static void main(String []args)
+	{
+		System.out.println("1 rondom number"+Math.random());
+		System.out.println("2nd Random Number: " + Math.random());  
+		System.out.println("3rd Random Number: " + Math.random());   
+		System.out.println("4th Random Number: " + Math.random());   
+	}
+	
+
+}
