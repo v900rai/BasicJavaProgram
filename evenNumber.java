75c591c3f35d4e52fa7ea1f71071d52e7fbd7bd5
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-06-19 23:52:25 +0530

    Add files via upload

--- evenNumber.java
+++ evenNumber.java
@@ -1,18 +1,17 @@
-package NumberProgram;
+package target200qQQQTTOOnnSS;
 
 public class evenNumber 
 {
-	public static void main(String[] args) 
+	public static void main(String []args)
 	{
-		int[] arr=new int[] {1,2,3,4,5,6,7,8,9};
-		System.out.println(" Enter the number");
-		for(int i=0; i<=arr.length; i++)
+		int number=100;
+		for(int i=0; i<=100; i++)
 		{
-			if(arr[i]%2==0)
+			if(i%2==0)
 			{
-				System.out.println("odd number"  +arr[i]);
+				System.out.println(i);
 			}
 		}
+	}
 
-}
 }
