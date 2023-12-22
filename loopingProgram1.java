8a61684fa1e5d5a0631f3dd2157be97f4558e04b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-12-22 12:36:20 +0530

    Add files via upload

---
+++ loopingProgram1.java
@@ -0,0 +1,16 @@
+package garaph;
+
+import java.util.Scanner;
+
+public class loopingProgram1 
+{
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int num=sc.nextInt();
+		for(int i=0; i<=num; i++) {
+			System.out.println("vishal");
+		}
+	}
+
+}
