550271d2721cdfc05b5e9ad3bb1484c69b14cf11
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-12-22 12:36:01 +0530

    Add files via upload

---
+++ loopineven.java
@@ -0,0 +1,13 @@
+package garaph;
+
+public class loopineven {
+	public static void main(String []args) {
+		int num=10;
+		for(int i=1; i<=num; i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
