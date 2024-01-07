f8f6a87132f5eb97b9f00da0a200e3f73570c86b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-07 12:49:26 +0530

    Add files via upload

---
+++ greaterNumber.java
@@ -0,0 +1,17 @@
+package january.targettt50;
+
+public class greaterNumber {
+	public static void main(String []args) {
+		int a=10 ,b=20 ,c=30;
+		if(a>=b && a>=c) {
+			System.out.println(a+"A grater then b and c");
+		}
+		 else if(b>=a && b>=c) {
+			System.out.println(b+" b grater then a&c ");
+			
+		}
+		else
+			System.out.println(c+" c is a grater");
+	}
+
+}
