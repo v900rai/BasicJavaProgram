b5d266d9297971caed190ab84030967558f55c5a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-04 19:42:43 +0530

    Add files via upload

---
+++ streamMain1.java
@@ -0,0 +1,13 @@
+package streamAPI;
+
+import java.util.List;
+
+public class streamMain1 {
+	public static void main(String []args)
+	{
+		List<Integer> list= new List<>();
+		list.add(10);
+		System.out.println(list);
+	}
+
+}
