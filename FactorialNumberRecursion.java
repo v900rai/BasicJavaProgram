61c6d258d25768aad3c034e0fd4ebc4c951fd11f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-06-03 21:50:33 +0530

    Add files via upload

---
+++ FactorialNumberRecursion.java
@@ -0,0 +1,22 @@
+package selfpractices;
+
+public class FactorialNumberRecursion {
+    static int factorial(int n){
+        if(n==1)
+            return 1;
+        
+        else 
+            retrun (n*factorial(n-1));
+
+
+
+
+
+
+}
+
+    public static void main(String[] args) {
+        System.out.println("factorial "+factorial(5));
+
+    }
+}
