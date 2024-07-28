18740c8ced255f6efc550627441674b746c9f847
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-07-28 16:46:31 +0530

    Add files via upload

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,29 +1,33 @@
-package october100;
+package AdvancedDataStructureInJava;
 
-public class BinarySearch 
-{
-	 public static void binarySearch(int arr[], int first, int last, int key){  
-		   int mid = (first + last)/2;  
-		   while( first <= last ){  
-		      if ( arr[mid] < key ){  
-		        first = mid + 1;     
-		      }else if ( arr[mid] == key ){  
-		        System.out.println("Element is found at index: " + mid);  
-		        break;  
-		      }else{  
-		         last = mid - 1;  
-		      }  
-		      mid = (first + last)/2;  
-		   }  
-		   if ( first > last ){  
-		      System.out.println("Element is not found!");  
-		   }  
-		 }  
-		 public static void main(String args[]){  
-		        int arr[] = {10,20,30,40,50};  
-		        int key = 30;  
-		        int last=arr.length-1;  
-		        binarySearch(arr,0,last,key);     
-		 }  
+public class BinarySearch {
+    public static int binarySearch(int arr[], int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + right / 2;
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 5, 7, 9};
+        int target = 7;
+        int result = binarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
 
 }
